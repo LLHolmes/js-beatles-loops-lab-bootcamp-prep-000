b928009026eaac7449af2f147991660833b8cf42
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-18 17:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var plays = []
+  for (var i = 0; i <= musicians.length; i++) {
+    plays.push(musicians[i] "plays" instruments[i])
+  }
+  return plays
+}
