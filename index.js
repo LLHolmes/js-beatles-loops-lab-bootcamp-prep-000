6514219eab5880c61bd33ca24790cf72b03bc9f9
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-18 18:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 function theBeatlesPlay(musicians, instruments) {
-  var plays = []
+  var plays = [];
   for (var i = 0; i < musicians.length; i++) {
-    plays.push(`${musicians[i]} plays ${instruments[i]}`)
+    plays.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return plays
 }
 
 function johnLennonFacts(facts) {
-  var x = 0
-  var excitedFacts = []
+  var x = 0;
+  var excitedFacts = [];
   while (x < facts.length) {
-    excitedFacts.push(`${facts[x]}!!!`)
-    x++
+    excitedFacts.push(`${facts[x]}!!!`);
+    x++;
   }
   return excitedFacts
+}
+
+function iLoveTheBeatles(number) {
+  var love = [];
+  do {
+    love.push("I love the Beatles");
+    number++;
+  } while (number < 15);
+  return love
 }
