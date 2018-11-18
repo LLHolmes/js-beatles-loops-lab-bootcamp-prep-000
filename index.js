7d83e4b02e32fadf85d5b1f1423527dae6b325c3
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-18 18:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var love = [];
   do {
-    love.push("I love the Beatles");
+    love.push("I love the Beatles!");
     number++;
   } while (number < 15);
   return love
